9ea49c28877c670e1c9be06f268b3b5164bc1d36
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-09-05 10:44:26 +0530

    feat: :chart_with_upwards_trend: add @vercel/analytics in the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
 				<Navbar />
 				{children}
 				<Footer />
+				<Analytics />
 			</body>
 		</html>
 	);
