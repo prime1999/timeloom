65ca4fc5fcd8a4c392b85422ec930000d2fd1b2c
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-09-10 16:58:05 +0530

    feat(auth): :lipstick: change UI to make footer always at the bottom of the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,9 +47,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className={`${inter.className} flex flex-col min-h-screen`}>
 				<Navbar />
-				{children}
+				<div className="grow">{children}</div>
 				<Footer />
 				<Toaster />
 				<Analytics />
