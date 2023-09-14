ea8db59dd2e76f09f3ccc95e89fdb32a4664ecda
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-09-15 01:42:38 +0530

    feat(auth): :sparkles: add api route to logout user

---
+++ app/api/users/logout/route.tsx
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+
+const GET = async () => {
+	try {
+		const response = NextResponse.json(
+			{
+				message: "Logged out successfully",
+				success: true,
+			},
+			{
+				status: 200,
+			}
+		);
+
+		response.cookies.set("token", "", {
+			httpOnly: true,
+			expires: new Date(0),
+		});
+
+		return response;
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json(
+			{
+				message: error.message,
+				success: false,
+			},
+			{
+				status: 500,
+			}
+		);
+	}
+};
+
+export { GET };
