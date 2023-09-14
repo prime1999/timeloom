6f4f23d11ab7a512f469564bf3c9c3a017161f98
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-09-15 02:44:25 +0530

    feat(auth): :sparkles: protect /login and /signup routes for logged in user trying to visit them

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse, NextRequest } from "next/server";
+import { jwtVerify } from "jose";
+
+export const config = {
+	matcher: ["/login", "/signup"],
+};
+
+export async function middleware(req: NextRequest) {
+	const token = req.cookies.get("token")?.value || "";
+	console.log("token: ", token);
+
+	if (!token) {
+		return NextResponse.next();
+	}
+
+	// const data: any = jwt.verify(token, process.env.TOKEN_SECRET!);
+	const { payload } = await jwtVerify(
+		token,
+		new TextEncoder().encode(process.env.TOKEN_SECRET!)
+	);
+
+	return NextResponse.redirect(new URL(`/${payload.username}`, req.url));
+}
